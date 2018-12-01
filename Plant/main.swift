39433c82354768bc1686a51d7081647c12d7ede1
Scott Tury <scott@scotts-macbook-pro.local>
2018-11-30 21:15:42 -0500

    Initial Commit

---
+++ Plant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Plant
+//
+//  Created by Scott Tury on 11/30/18.
+//  Copyright © 2018 Scott Tury. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
